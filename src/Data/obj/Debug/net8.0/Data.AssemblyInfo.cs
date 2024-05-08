7e52dcbca3014ae9bad15af267405f0b5a4fe16a
AdemBlel17 <ademblel@takiacademyteam.com>
2024-05-08 08:15:19 +0100

    Change dep injection to scope instead of singleton

--- src/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ src/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8d286f9653e5125deb57d31160851b9efd8ab4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
